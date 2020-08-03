aad1f10fd6a002b64af2aeea200780b47e27b1d3
Underdisc <connortdeakin@gmail.com>
2020-08-03 22:36:27 +0200

    Some uncommitted changes that have not made their way off of my laptop.

---
+++ src/framer.h
@@ -0,0 +1,21 @@
+#ifndef Framer_h
+#define Framer_h
+
+#include <chrono>
+
+class Framer
+{
+public:
+    static void Init(int framerate);
+    static void SetFramerate(int framerate);
+    static void Start();
+    static void End();
+
+    typedef std::chrono::system_clock system_clock;
+    typedef std::chrono::time_point<system_clock> time_point;
+    //float
+    time_point _frame_start;
+    time_point _frame_end;
+};
+
+#endif
