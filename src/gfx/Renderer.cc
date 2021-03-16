0012aa7cb30f6f85b656edde6c77c215f77a46be
Underdisc <connortdeakin@gmail.com>
2021-03-17 00:06:55 +0100

    This is it. This is the fucking commit that strips away all the pointless shit I have kept in main for months.

--- src/gfx/Renderer.cc
+++ src/gfx/Renderer.cc
@@ -24,6 +24,10 @@ void Init()
   Vec3 color = {0.0f, 1.0f, 0.0f};
   nDefaultShader.SetVec3("uColor", color.CData());
 
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
