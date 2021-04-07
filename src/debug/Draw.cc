5cbccffc2aef7fa9c0ec2bf5738d317b1cb6f242
Underdisc <connortdeakin@gmail.com>
2021-04-07 20:52:37 +0100

    Debug::Draw now supports point drawing. The size of points and width of lines has also been increased.

--- src/debug/Draw.cc
+++ src/debug/Draw.cc
@@ -27,22 +27,43 @@ void Init()
   Gfx::Shader::InitResult result =
     nShader.Init("vres/shader/DebugLine.vs", "vres/shader/Color.fs");
   LogAbortIf(!result.mSuccess, result.mError.c_str());
+
+  glPointSize(8.0f);
+  glLineWidth(4.0f);
 }
 
-void Line(const Vec3& a, const Vec3& b, const Vec3& color)
+void Point(const Vec3& point, const Vec3& color)
 {
-  Vec3 vertexBuffer[2] = {a, b};
   unsigned int vao, vbo;
   glGenVertexArrays(1, &vao);
   glGenBuffers(1, &vbo);
 
   glBindVertexArray(vao);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vec3), point.CData(), GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vec3), (void*)0);
+  glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  nRenderables.Push({vao, vbo, 1, color});
+}
+
+void Line(const Vec3& a, const Vec3& b, const Vec3& color)
+{
+  unsigned int vao, vbo;
+  glGenVertexArrays(1, &vao);
+  glGenBuffers(1, &vbo);
+
+  glBindVertexArray(vao);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  Vec3 vertexBuffer[2] = {a, b};
   glBufferData(
     GL_ARRAY_BUFFER, sizeof(vertexBuffer), vertexBuffer, GL_STREAM_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vec3), (void*)0);
   glEnableVertexAttribArray(0);
   glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
 
   nRenderables.Push({vao, vbo, 2, color});
 }
@@ -67,12 +88,14 @@ void Render(const Mat4& view, const Mat4& projection)
     const Renderable& renderable = nRenderables[i];
     nShader.SetVec3("uColor", renderable.mColor.CData());
     glBindVertexArray(renderable.mVao);
-    glDrawArrays(GL_LINES, 0, renderable.mCount);
-  }
-
-  for (int i = 0; i < nRenderables.Size(); ++i)
-  {
-    const Renderable& renderable = nRenderables[i];
+    if (renderable.mCount == 1)
+    {
+      glDrawArrays(GL_POINTS, 0, renderable.mCount);
+    } else
+    {
+      glDrawArrays(GL_LINES, 0, renderable.mCount);
+    }
+    glBindVertexArray(0);
     glDeleteVertexArrays(1, &renderable.mVao);
     glDeleteBuffers(1, &renderable.mVbo);
   }
--- src/debug/Draw.h
+++ src/debug/Draw.h
@@ -5,6 +5,7 @@ namespace Debug {
 namespace Draw {
 
 void Init();
+void Point(const Vec3& point, const Vec3& color);
 void Line(const Vec3& a, const Vec3& b, const Vec3& color);
 void CartesianAxes();
 void Render(const Mat4& view, const Mat4& proj);
