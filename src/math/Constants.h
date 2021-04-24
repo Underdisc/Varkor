1a4d99e2fd033b1bcd012c981207b0dec83c2723
Underdisc <connortdeakin@gmail.com>
2021-04-24 15:45:09 +0100

    The epsilon values have been adjusted.

--- src/math/Constants.h
+++ src/math/Constants.h
@@ -6,8 +6,8 @@ namespace Math {
 constexpr float nPi = 3.14159265f;
 constexpr float nPiO2 = 1.57079632f;
 constexpr float nTao = 6.28318530f;
-constexpr float nEpsilon = 0.00000001f;
-constexpr float nEpsilonL = 0.00001f;
+constexpr float nEpsilon = 0.00001f;
+constexpr float nEpsilonL = 0.001f;
 
 } // namespace Math
 
