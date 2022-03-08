e4c9f8fec394202285db55fa83d4eb622cfe3b79
Underdisc <connortdeakin@gmail.com>
2022-03-08 13:14:13 +0100

    The project is now compiled under the c++17 standard.
    This is because std::filesystem, which is only available in c++17, is being used. The code the uses it will come in a later commit.

--- src/ds/Map.h
+++ src/ds/Map.h
@@ -34,8 +34,8 @@ template<typename K, typename V>
 class Map: public RbTree<KvPair<K, V>>
 {
 public:
-  typedef RbTree<KvPair<K, V>>::Iter Iter;
-  typedef RbTree<KvPair<K, V>>::CIter CIter;
+  typename typedef RbTree<KvPair<K, V>>::Iter Iter;
+  typename typedef RbTree<KvPair<K, V>>::CIter CIter;
   Iter begin() const;
   Iter end() const;
   CIter cbegin() const;
