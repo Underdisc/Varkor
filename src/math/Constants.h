5b6b0e4c2408315b1def61a10271ae4f4d5789f5
Underdisc <connortdeakin@gmail.com>
2022-05-07 14:06:00 +0200

    Whoops, I spelled Tau wrong.

--- src/math/Constants.h
+++ src/math/Constants.h
@@ -5,7 +5,7 @@ namespace Math {
 
 constexpr float nPi = 3.14159265f;
 constexpr float nPiO2 = 1.57079632f;
-constexpr float nTao = 6.28318530f;
+constexpr float nTau = 6.28318530f;
 constexpr float nEpsilon = 0.00001f;
 constexpr float nEpsilonL = 0.001f;
 
