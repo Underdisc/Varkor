Underdisc/Varkor

commit 05e1c2deccc2588ee206902c1088b3ce784340d6

Varkor projects no longer require files to be copied into their directory.
Instead of a Varkor project having its own working directory, the project's executable is placed in Varkor's working directory. This way the executable sits alongside any required dlls and the vres/ directory.

Though copying files is not required, this did require the addition of a project directory option because the exectable needs to know where it can find all of the resource files that are specific to a project.

src/editor/FileInterface.cc
@@ -1,6 +1,7 @@
 #include <dirent/dirent.h>
 #include <imgui/imgui.h>
 
+#include "Options.h"
 #include "editor/FileInterface.h"
 #include "editor/Utility.h"
 
@@ -24,15 +25,17 @@ void FileInterface::Show()
   // Open the currently selected directory. We unroll the path to handle cases
   // where directories may have been deleted or renamed.
   static std::string path = ".";
-  DIR* directory = opendir(path.c_str());
+  std::string fullPath = Options::PrependResDirectory(path);
+  DIR* directory = opendir(fullPath.c_str());
   while (directory == nullptr)
   {
     path.erase(path.find_last_of('/'));
-    directory = opendir(path.c_str());
+    fullPath.erase(path.find_last_of('/'));
+    directory = opendir(fullPath.c_str());
   }
 
-  // This will prevent "." from showing up as an option and ".." when the
-  // current path is ".".
+  // This will prevent "."  and ".." from showing up as options when the current
+  // path is ".".
   readdir(directory);
   if (path == ".")
   {
@@ -95,13 +98,7 @@ void FileInterface::Show()
   }
   if (ImGui::Button(buttonLabel, ImVec2(-1, 0)))
   {
-    if (path.size() > 1)
-    {
-      mCallback(path.substr(2) + "/" + mFile);
-    } else
-    {
-      mCallback(mFile);
-    }
+    mCallback(fullPath.substr(2) + "/" + mFile);
     mOpen = false;
   }
   ImGui::End();

.gitignore
@@ -3,10 +3,11 @@
 *.ilk
 *.pdb
 buildSpecs.bat
+projects.cmake
 .vs/
 build/
 working/imgui.ini
-working/assets.vlk
-working/res/
+working/res/assets.vlk
+working/res/test
 working/test/*/coverage
 working/test/*/out_diff.txt

CMakeLists.txt
@@ -41,3 +41,17 @@ AddVarkorTarget()
 
 # Create all of the test targets.
 add_subdirectory(src/test)
+
+function(AddProject targetName directory)
+  project(${targetName})
+  add_executable(${targetName})
+  add_subdirectory("${directory}/src" "${targetName}Src")
+  target_link_libraries(${targetName} varkorLibrary)
+  target_compile_definitions(${targetName}
+    PUBLIC PROJECT_DIRECTORY="${directory}")
+  set_property(TARGET ${targetName} PROPERTY RUNTIME_OUTPUT_DIRECTORY
+    "${CMAKE_SOURCE_DIR}/working")
+endfunction()
+
+# Create all of the targets for different Varkor projects.
+include("projects.cmake" OPTIONAL)

src/AssetLibrary.cc
@@ -1,6 +1,7 @@
 #include <fstream>
 
 #include "AssetLibrary.h"
+#include "Options.h"
 #include "Viewport.h"
 #include "gfx/Font.h"
 #include "gfx/Image.h"
@@ -152,11 +153,12 @@ void Purge()
   }
 }
 
-const char* nAssetFile = "assets.vlk";
+const char* nAssetsFilename = "assets.vlk";
 void DeserializeAssets()
 {
   Vlk::Value rootVal;
-  Result result = rootVal.Read(nAssetFile);
+  std::string assetsFile = Options::PrependResDirectory(nAssetsFilename);
+  Result result = rootVal.Read(assetsFile.c_str());
   if (!result.Success())
   {
     LogError(result.mError.c_str());
@@ -176,7 +178,8 @@ void SerializeAssets()
   SerializeAssets<Gfx::Image>(rootVal);
   SerializeAssets<Gfx::Model>(rootVal);
   SerializeAssets<Gfx::Shader>(rootVal);
-  Result result = rootVal.Write(nAssetFile);
+  std::string assetsFile = Options::PrependResDirectory(nAssetsFilename);
+  Result result = rootVal.Write(assetsFile.c_str());
   LogErrorIf(!result.Success(), result.mError.c_str());
 }
 
@@ -237,8 +240,9 @@ void AssetBin<Gfx::Model>::InitDefault(Args&&... args)
 {
   Asset<Gfx::Model>& defaultAsset = AssetBin<Gfx::Model>::smAssets.Emplace(
     nDefaultAssetId, "Default", Status::Initializing);
+  defaultAsset.SetPaths(args...);
   InitBin<Gfx::Model>::smInitQueue.Push(nDefaultAssetId);
-  Result result = defaultAsset.mResource.Init(args...);
+  Result result = defaultAsset.Init();
   InitBin<Gfx::Model>::smInitQueue.Pop();
   LogAbortIf(!result.Success(), result.mError.c_str());
 }
@@ -250,8 +254,9 @@ AssetId AssetBin<Gfx::Model>::Require(const std::string& name, Args&&... args)
   AssetId id = AssetBin<Gfx::Model>::NextRequiredId();
   Asset<Gfx::Model>& newAsset =
     AssetBin<Gfx::Model>::smAssets.Emplace(id, name, Status::Initializing);
+  newAsset.SetPaths(args...);
   InitBin<Gfx::Model>::smInitQueue.Push(id);
-  Result result = newAsset.mResource.Init(args...);
+  Result result = newAsset.Init();
   InitBin<Gfx::Model>::smInitQueue.Pop();
   LogAbortIf(!result.Success(), result.mError.c_str());
   return id;

src/AssetLibrary.h
@@ -46,7 +46,8 @@ public:
   T mResource;
   Asset(const std::string& name, Status status);
   Result Init();
-  Result Init(const std::string paths[T::smInitPathCount]);
+  template<typename... Paths>
+  void SetPaths(const Paths&... paths);
   void SetPath(int index, const std::string& newPath);
   const std::string& GetPath(int index) const;
 
@@ -54,6 +55,14 @@ public:
 
 private:
   std::string mPaths[T::smInitPathCount];
+
+  template<typename Path>
+  void SetPaths(int currentPathIndex, const Path& path);
+  template<typename Path, typename... RemainingPaths>
+  void SetPaths(
+    int currentPathIndex,
+    const Path& path,
+    const RemainingPaths&... remainingPaths);
 };
 
 // These functions serve as the primary interface for the AssetLibrary.

src/AssetLibrary.hh
@@ -1,3 +1,8 @@
+#include <filesystem>
+#include <sstream>
+
+#include "Options.h"
+
 namespace Gfx {
 struct Model;
 }
@@ -12,17 +17,53 @@ Asset<T>::Asset(const std::string& name, Status status):
 template<typename T>
 Result Asset<T>::Init()
 {
-  return mResource.Init(mPaths);
+  // These path checks and conversions are necessary because a path may be
+  // relative to the working directory (consider files within vres/) or a
+  // project's resource directory.
+  std::string paths[T::smInitPathCount];
+  for (int i = 0; i < T::smInitPathCount; ++i)
+  {
+    if (std::filesystem::exists(mPaths[i]))
+    {
+      paths[i] = mPaths[i];
+      continue;
+    }
+    std::string projectAssetPath = Options::PrependResDirectory(mPaths[i]);
+    if (!std::filesystem::exists(projectAssetPath))
+    {
+      std::stringstream error;
+      error << "Failed to open \"" << mPaths[i] << "\".";
+      return Result(error.str());
+    }
+    paths[i] = projectAssetPath;
+  }
+  return mResource.Init(paths);
 }
 
 template<typename T>
-Result Asset<T>::Init(const std::string paths[T::smInitPathCount])
+template<typename Path>
+void Asset<T>::SetPaths(int currentPathIndex, const Path& path)
 {
-  for (size_t i = 0; i < T::smInitPathCount; ++i)
-  {
-    mPaths[i] = paths[i];
-  }
-  return Init();
+  mPaths[currentPathIndex] = path;
+}
+
+template<typename T>
+template<typename Path, typename... RemainingPaths>
+void Asset<T>::SetPaths(
+  int currentPathIndex,
+  const Path& path,
+  const RemainingPaths&... remainingPaths)
+{
+  mPaths[currentPathIndex] = path;
+  ++currentPathIndex;
+  SetPaths(currentPathIndex, remainingPaths...);
+}
+
+template<typename T>
+template<typename... Paths>
+void Asset<T>::SetPaths(const Paths&... paths)
+{
+  SetPaths(0, paths...);
 }
 
 template<typename T>
@@ -68,8 +109,9 @@ void AssetBin<T>::InitDefault(Args&&... args)
 {
   Asset<T>& defaultAsset =
     AssetBin<T>::smAssets.Emplace(nDefaultAssetId, "Default", Status::Live);
+  defaultAsset.SetPaths(args...);
   InitBin<T>::smInitQueue.Push(nDefaultAssetId);
-  Result result = defaultAsset.mResource.Init(args...);
+  Result result = defaultAsset.Init();
   InitBin<T>::smInitQueue.Pop();
   LogAbortIf(!result.Success(), result.mError.c_str());
 }
@@ -83,8 +125,9 @@ AssetId AssetBin<T>::Require(const std::string& name, Args&&... args)
 {
   AssetId id = NextRequiredId();
   Asset<T>& newAsset = smAssets.Emplace(id, name, Status::Live);
+  newAsset.SetPaths(args...);
   InitBin<T>::smInitQueue.Push(id);
-  Result result = newAsset.mResource.Init(args...);
+  Result result = newAsset.Init();
   InitBin<T>::smInitQueue.Pop();
   LogAbortIf(!result.Success(), result.mError.c_str());
   return id;

src/Options.cc
@@ -6,6 +6,7 @@
 namespace Options {
 
 std::string nLoadSpace = "";
+std::string nProjectDirectory = "";
 
 void ShowHelp()
 {
@@ -15,8 +16,14 @@ void ShowHelp()
        "and select the space saved within the given file.\n";
 }
 
-Result Init(int argc, char* argv[])
+Result Init(int argc, char* argv[], const char* projectDirectory)
 {
+  nProjectDirectory = projectDirectory;
+  if (!nProjectDirectory.empty() && nProjectDirectory.back() != '/')
+  {
+    nProjectDirectory += '/';
+  }
+
   const char* getoptString = "hs:";
   option allOptions[] = {
     {"help", no_argument, NULL, 'h'},
@@ -34,6 +41,11 @@ Result Init(int argc, char* argv[])
     }
   }
   return Result();
+}
+
+std::string PrependResDirectory(const std::string& path)
+{
+  return nProjectDirectory + "res/" + path;
 }
 
 } // namespace Options

src/Options.h
@@ -8,8 +8,10 @@
 namespace Options {
 
 extern std::string nLoadSpace;
+extern std::string nProjectDirectory;
 
-Result Init(int argc, char* argv[]);
+Result Init(int argc, char* argv[], const char* projectDirectory);
+std::string PrependResDirectory(const std::string& path);
 
 } // namespace Options
 

commit 70959da1f597e93373c04c2c3f1292d5c072492c

Fixed FileInterface bug introduced in 05e1c2d.
The path unrolling did not work because the incorrect string was for determining how much of the path should be trimmed.

src/editor/FileInterface.cc
@@ -46,7 +46,7 @@ void FileInterface::Show()
   DIR* directory = opendir(fullPath.c_str());
   while (directory == nullptr) {
     path.erase(path.find_last_of('/'));
-    fullPath.erase(path.find_last_of('/'));
+    fullPath.erase(fullPath.find_last_of('/'));
     directory = opendir(fullPath.c_str());
   }
 