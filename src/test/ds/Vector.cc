7530f6cec3f16737c0326aed97ebc127f8df11c7
Underdisc <connortdeakin@gmail.com>
2022-07-31 18:26:07 +0200

    Cleaned up the Ds::Vector unit test.
    The test now has a common RunTest function that some other tests have. This gets rid of the redundant code that's repeated for nearly every test. I also removed all use of std::endl from the tests.

--- src/test/ds/Vector.cc
+++ src/test/ds/Vector.cc
@@ -9,8 +9,7 @@
 
 void CopyConstructor0()
 {
-  std::cout << "<= CopyConstructor0 =>" << std::endl;
-  TestType::ResetCounts();
+  std::cout << "<= CopyConstructor0 =>\n";
   Ds::Vector<TestType> test;
   for (int i = 0; i < 15; ++i) {
     test.Emplace(15 - i);
@@ -21,14 +20,13 @@ void CopyConstructor0()
   std::cout << "<- copy ->\n";
   PrintVector(copy);
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void CopyConstructor1()
 {
   // This will ensure that copied vector elements are constructed and not merely
   // assigned to from the original vector.
-  std::cout << "<= CopyConstructor1 =>" << std::endl;
+  std::cout << "<= CopyConstructor1 =>\n";
   Ds::Vector<std::string> test;
   for (int i = 0; i < 10; ++i) {
     std::string newElement;
@@ -40,12 +38,11 @@ void CopyConstructor1()
   PrintVector(test);
   std::cout << "<- copy ->\n";
   PrintVector(copy);
-  std::cout << "\n";
 }
 
 void MoveConstructor()
 {
-  std::cout << "<= MoveConstructor =>" << std::endl;
+  std::cout << "<= MoveConstructor =>\n";
   Ds::Vector<int> ogVector;
   for (int i = 0; i < 20; ++i) {
     ogVector.Push(i);
@@ -55,13 +52,12 @@ void MoveConstructor()
   const int* moveVectorPtr = moveVector.CData();
   PrintVector(ogVector);
   PrintVector(moveVector);
-  std::cout << "Moved: " << (ogVectorPtr == moveVectorPtr) << std::endl;
-  std::cout << std::endl;
+  std::cout << "Moved: " << (ogVectorPtr == moveVectorPtr) << '\n';
 }
 
 void SinglePush()
 {
-  std::cout << "<= SinglePush =>" << std::endl;
+  std::cout << "<= SinglePush =>\n";
   Ds::Vector<int> testVector;
   PrintVector(testVector);
   for (int i = 0; i < 5; ++i) {
@@ -72,12 +68,11 @@ void SinglePush()
     testVector.Push(i);
   }
   PrintVector(testVector);
-  std::cout << std::endl;
 }
 
 void MovePush()
 {
-  std::cout << "<= MovePush =>" << std::endl;
+  std::cout << "<= MovePush =>\n";
   // We create a vector of vectors and save the pointers to the data for each of
   // the inner vectors.
   Ds::Vector<Ds::Vector<int>> testVector;
@@ -104,36 +99,33 @@ void MovePush()
   for (int i = 0; i < innerVectorCount; ++i) {
     std::cout << (ogInnerPtrs[i] == movedInnerPtrs[i]);
   }
-  std::cout << std::endl << std::endl;
+  std::cout << '\n';
 }
 
 void MultiplePush()
 {
-  std::cout << "<= MultiplePush =>" << std::endl;
+  std::cout << "<= MultiplePush =>\n";
   Ds::Vector<int> test;
   test.Push(0, 30);
   PrintVector(test);
   test.Push(1, 15);
   PrintVector(test);
-  std::cout << std::endl;
 }
 
 void Emplace()
 {
-  std::cout << "<= Emplace =>" << std::endl;
+  std::cout << "<= Emplace =>\n";
   Ds::Vector<TestType> testVector;
   for (int i = 0; i < 15; ++i) {
     testVector.Emplace(i, (float)i);
   }
   PrintVector(testVector);
-  std::cout << std::endl;
 }
 
 void Insert0()
 {
   // Test insterting a new element at the end of the vector.
-  std::cout << "<= Insert0 =>" << std::endl;
-  TestType::ResetCounts();
+  std::cout << "<= Insert0 =>\n";
   Ds::Vector<TestType> test;
   for (int i = 0; i < Ds::Vector<TestType>::smStartCapacity; ++i) {
     test.Emplace(i);
@@ -141,14 +133,12 @@ void Insert0()
   test.Insert(Ds::Vector<int>::smStartCapacity, TestType(-1));
   PrintVector(test);
   TestType::PrintCounts();
-  std::cout << std::endl;
 }
 
 void Insert1()
 {
   // Test inserting new elments in the middle of the vector.
   std::cout << "<= Insert1 =>\n";
-  TestType::ResetCounts();
   Ds::Vector<TestType> test;
   for (int i = 0; i < Ds::Vector<TestType>::smStartCapacity; ++i) {
     test.Emplace(i);
@@ -158,57 +148,50 @@ void Insert1()
   }
   PrintVector(test);
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void Insert2()
 {
   // Insert strings to ensure appropriate construction and destruction.
-  std::cout << "<= Insert2 =>" << std::endl;
+  std::cout << "<= Insert2 =>\n";
   Ds::Vector<std::string> test;
   test.Insert(0, "string 0");
   test.Insert(0, "string 1");
   test.Insert(0, "string 2");
   PrintVector(test);
-  std::cout << '\n';
 }
 
 void Pop()
 {
-  std::cout << "<= Pop =>" << std::endl;
+  std::cout << "<= Pop =>\n";
   Ds::Vector<TestType> testVector;
   for (int i = 0; i < 10; ++i) {
     testVector.Emplace(i, (float)i);
   }
 
-  TestType::ResetCounts();
   for (int i = 0; i < 5; ++i) {
     testVector.Pop();
   }
   PrintVector(testVector);
-  std::cout << "DestructorCount: " << TestType::smDestructorCount << std::endl
-            << std::endl;
+  TestType::PrintCounts();
 }
 
 void Clear()
 {
-  std::cout << "<= Clear =>" << std::endl;
+  std::cout << "<= Clear =>\n";
   Ds::Vector<TestType> testVector;
   for (int i = 0; i < 5; ++i) {
     testVector.Emplace(i, (float)i);
   }
 
-  TestType::ResetCounts();
   testVector.Clear();
   PrintVector(testVector);
-  std::cout << "DestructorCount: " << TestType::smDestructorCount << std::endl
-            << std::endl;
+  TestType::PrintCounts();
 }
 
 void Remove0()
 {
-  std::cout << "<= Remove0 =>" << std::endl;
-  TestType::ResetCounts();
+  std::cout << "<= Remove0 =>\n";
   Ds::Vector<TestType> testVector;
   for (int i = 0; i < 10; ++i) {
     testVector.Emplace(i, (float)i);
@@ -223,7 +206,6 @@ void Remove0()
   }
   PrintVector(testVector, false);
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void Remove1()
@@ -239,18 +221,16 @@ void Remove1()
   test.Remove(1);
   test.Remove(0);
   PrintVector(test);
-  std::cout << "\n";
 }
 
 void LazyRemove()
 {
-  std::cout << "<= LazyRemove =>" << std::endl;
+  std::cout << "<= LazyRemove =>\n";
   Ds::Vector<TestType> testVector;
   for (int i = 0; i < 10; ++i) {
     testVector.Emplace(i, (float)i);
   }
 
-  TestType::ResetCounts();
   for (int i = 8; i >= 0; i -= 2) {
     testVector.LazyRemove(i);
   }
@@ -259,34 +239,29 @@ void LazyRemove()
     testVector.LazyRemove(0);
   }
   PrintVector(testVector, false);
-  std::cout << "DestructorCount: " << TestType::smDestructorCount << std::endl
-            << "MoveAssignmentCount: " << TestType::smMoveAssignmentCount
-            << std::endl
-            << std::endl;
+  TestType::PrintCounts();
 }
 
 void IndexOperator()
 {
-  std::cout << "<= IndexOperator =>" << std::endl;
+  std::cout << "<= IndexOperator =>\n";
   Ds::Vector<int> testVector;
   for (int i = 0; i < 20; ++i) {
     testVector.Push(i);
   }
 
   int val0 = testVector[5];
-  std::cout << "[5]: " << val0 << std::endl;
+  std::cout << "[5]: " << val0 << '\n';
   const int& val1 = testVector[10];
-  std::cout << "[10]: " << val1 << std::endl;
+  std::cout << "[10]: " << val1 << '\n';
   testVector[15] = 7;
-  std::cout << "[15]: " << testVector[15] << std::endl;
-  std::cout << std::endl;
+  std::cout << "[15]: " << testVector[15] << '\n';
 }
 
 void CopyAssignment0()
 {
   // Test copying a vector to another vector with the same size.
   std::cout << "<= CopyAssignment0 =>\n";
-  TestType::ResetCounts();
   Ds::Vector<TestType> test, copy;
   for (int i = 0; i < 5; ++i) {
     test.Emplace(i);
@@ -298,7 +273,6 @@ void CopyAssignment0()
   std::cout << "<- copy ->\n";
   PrintVector(copy);
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void CopyAssignment1()
@@ -306,7 +280,6 @@ void CopyAssignment1()
   // Test the case where the size of the vector being copied to is larger than
   // the size of the vector being copied from.
   std::cout << "<= CopyAssignment1 =>\n";
-  TestType::ResetCounts();
   Ds::Vector<TestType> test, copy;
   for (int i = 0; i < 5; ++i) {
     test.Emplace(i);
@@ -320,7 +293,6 @@ void CopyAssignment1()
   std::cout << "<- copy ->\n";
   PrintVector(copy);
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void CopyAssignment2()
@@ -329,7 +301,6 @@ void CopyAssignment2()
   // vector being copied from and the capacity of the vector being copied to is
   // greater than or equal to the size of the vector being copied from.
   std::cout << "<= CopyAssignment2 =>\n";
-  TestType::ResetCounts();
   Ds::Vector<TestType> test, copy;
   for (int i = 0; i < 7; ++i) {
     test.Emplace(i);
@@ -343,7 +314,6 @@ void CopyAssignment2()
   std::cout << "<- copy ->\n";
   PrintVector(copy);
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void CopyAssignment3()
@@ -351,7 +321,6 @@ void CopyAssignment3()
   // Test the case where the vector being copied to has a capacity smaller than
   // the size of the vector being copied from.
   std::cout << "<= CopyAssignment3 =>\n";
-  TestType::ResetCounts();
   Ds::Vector<TestType> test, copy;
   for (int i = 0; i < 15; ++i) {
     test.Emplace(i);
@@ -363,12 +332,11 @@ void CopyAssignment3()
   std::cout << "<- copy ->\n";
   PrintVector(copy);
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void MoveAssignment()
 {
-  std::cout << "<= MoveAssignment =>" << std::endl;
+  std::cout << "<= MoveAssignment =>\n";
   // We push values into the vector being moved from and the vector being moved
   // to to ensure that the data in the vector being moved to is deallocated.
   Ds::Vector<int> ogVector;
@@ -384,13 +352,12 @@ void MoveAssignment()
   const int* moveVectorPtr = moveVector.CData();
   PrintVector(ogVector);
   PrintVector(moveVector);
-  std::cout << "Moved: " << (ogVectorPtr == moveVectorPtr) << std::endl;
-  std::cout << std::endl;
+  std::cout << "Moved: " << (ogVectorPtr == moveVectorPtr) << '\n';
 }
 
 void Find()
 {
-  std::cout << "<= Find =>" << std::endl;
+  std::cout << "<= Find =>\n";
   Ds::Vector<int> test;
   for (int i = 0; i < 20; ++i) {
     if (i % 2 == 0) {
@@ -399,26 +366,24 @@ void Find()
   }
   std::cout << "4: " << test.Find(4) << '\n';
   std::cout << "12: " << test.Find(12) << '\n';
-  std::cout << "13: " << test.Find(13) << "\n\n";
+  std::cout << "13: " << test.Find(13) << '\n';
 }
 
 void Contains()
 {
-  std::cout << "<= Contains =>" << std::endl;
+  std::cout << "<= Contains =>\n";
   Ds::Vector<int> testVector;
   for (int i = 0; i < 20; ++i) {
     testVector.Push(i);
   }
-  std::cout << "10: " << testVector.Contains(10) << std::endl;
-  std::cout << "21: " << testVector.Contains(21) << std::endl;
-  std::cout << std::endl;
+  std::cout << "10: " << testVector.Contains(10) << '\n';
+  std::cout << "21: " << testVector.Contains(21) << '\n';
 }
 
 void Resize0()
 {
   // Test the case where the new size is smaller than the current size;
-  std::cout << "<= Resize0 =>" << std::endl;
-  TestType::ResetCounts();
+  std::cout << "<= Resize0 =>\n";
   Ds::Vector<TestType> test;
   for (int i = 0; i < 10; ++i) {
     test.Emplace(i);
@@ -426,14 +391,12 @@ void Resize0()
   test.Resize(5);
   PrintVector(test);
   TestType::PrintCounts();
-  std::cout << std::endl;
 }
 
 void Resize1()
 {
   // Test the case where the new size is larger than the current capacity.
   std::cout << "<= Resize1 =>\n";
-  TestType::ResetCounts();
   Ds::Vector<TestType> test;
   for (int i = 0; i < 10; ++i) {
     test.Emplace(i);
@@ -441,7 +404,6 @@ void Resize1()
   test.Resize(20, TestType(-1));
   PrintVector(test);
   TestType::PrintCounts();
-  std::cout << std::endl;
 }
 
 void Resize2()
@@ -457,12 +419,11 @@ void Resize2()
   test.Resize(20, "a");
   test.Resize(10);
   PrintVector(test);
-  std::cout << "\n";
 }
 
 void Reserve()
 {
-  std::cout << "<= Reserve =>" << std::endl;
+  std::cout << "<= Reserve =>\n";
   // We first test the case where we give an empty vector some capacity and then
   // we make sure reserving a smaller capacity doesn't change the capacity of
   // the vector.
@@ -480,7 +441,6 @@ void Reserve()
   }
   test1.Reserve(30);
   PrintVector(test1);
-  std::cout << std::endl;
 }
 
 void Shrink0()
@@ -488,7 +448,6 @@ void Shrink0()
   // We perform two consecutive shrinks. The first makes the capacity and size
   // equal. Since capacity and size are equal, the second does nothing.
   std::cout << "<= Shrink0 =>\n";
-  TestType::ResetCounts();
   Ds::Vector<TestType> test;
   for (int i = 0; i < 5; ++i) {
     test.Emplace(i);
@@ -496,14 +455,13 @@ void Shrink0()
   test.Shrink();
   test.Shrink();
   TestType::PrintCounts();
-  std::cout << "\n";
 }
 
 void Shrink1()
 {
   // This will crash or cause memory leaks without the proper use of
   // constructors and destructors.
-  std::cout << "<= Shrink1 =>" << std::endl;
+  std::cout << "<= Shrink1 =>\n";
   Ds::Vector<std::string> test;
   for (int i = 0; i < 5; ++i) {
     std::string newString;
@@ -512,12 +470,11 @@ void Shrink1()
   }
   test.Shrink();
   PrintVector(test);
-  std::cout << std::endl;
 }
 
 void CData()
 {
-  std::cout << "<= CData =>" << std::endl;
+  std::cout << "<= CData =>\n";
   Ds::Vector<int> test;
   for (int i = 0; i < 5; ++i) {
     test.Push(i);
@@ -527,31 +484,29 @@ void CData()
   for (int i = 0; i < 4; ++i) {
     std::cout << data[i] << ", ";
   }
-  std::cout << data[4] << "]" << std::endl;
-  std::cout << std::endl;
+  std::cout << data[4] << "]\n";
 }
 
 void Top()
 {
-  std::cout << "<= Top =>" << std::endl;
+  std::cout << "<= Top =>\n";
   Ds::Vector<int> test;
   for (int i = 0; i < 10; ++i) {
     test.Push(i * 2);
   }
-  std::cout << test.Top() << std::endl;
+  std::cout << test.Top() << '\n';
   for (int i = 0; i < 5; ++i) {
     test.Pop();
   }
-  std::cout << test.Top() << std::endl;
+  std::cout << test.Top() << '\n';
   test.Pop();
   test.Top() = 20;
-  std::cout << test.Top() << std::endl;
-  std::cout << std::endl;
+  std::cout << test.Top() << '\n';
 }
 
 void InnerVector()
 {
-  std::cout << "<= InnerVector =>" << std::endl;
+  std::cout << "<= InnerVector =>\n";
   Ds::Vector<Ds::Vector<TestType>> testVector;
 
   for (int i = 0; i < 5; ++i) {
@@ -566,12 +521,11 @@ void InnerVector()
   for (int i = 0; i < 5; ++i) {
     PrintVector(testVector[i], false);
   }
-  std::cout << std::endl;
 }
 
 void BigInnerVector()
 {
-  std::cout << "<= BigInnerVector =>" << std::endl;
+  std::cout << "<= BigInnerVector =>\n";
   // We create one vector that will contain 100 Vectors and each of those will
   // have 100 TestType values. Besides being a minor stress test, this also
   // ensures that the outer vector grows without crashing or losing any of the
@@ -587,21 +541,19 @@ void BigInnerVector()
   }
 
   std::cout << "Main Vector Stats: [" << testVector.Size() << ", "
-            << testVector.Capacity() << "]" << std::endl
-            << "[0][0]: " << testVector[0][0] << std::endl
-            << "[25][25]: " << testVector[25][25] << std::endl
-            << "[50][50]: " << testVector[50][50] << std::endl
-            << "[75][75]: " << testVector[75][75] << std::endl
-            << "[99][99]: " << testVector[99][99] << std::endl
-            << std::endl;
+            << testVector.Capacity() << "]\n"
+            << "[0][0]: " << testVector[0][0] << '\n'
+            << "[25][25]: " << testVector[25][25] << '\n'
+            << "[50][50]: " << testVector[50][50] << '\n'
+            << "[75][75]: " << testVector[75][75] << '\n'
+            << "[99][99]: " << testVector[99][99] << '\n';
 }
 
 void ConstructionDestructionCounts()
 {
-  std::cout << "<= ConstructionDestructionCounts =>" << std::endl;
+  std::cout << "<= ConstructionDestructionCounts =>\n";
   // We reset the counts on TestType and perform a good chunk of vector
   // operations to see how many times constructors and destructors are called.
-  TestType::ResetCounts();
   Ds::Vector<Ds::Vector<TestType>> testVector;
   for (int i = 0; i < 30; ++i) {
     Ds::Vector<TestType> inner;
@@ -612,61 +564,67 @@ void ConstructionDestructionCounts()
     testVector.Push(std::move(inner));
   }
   testVector.Clear();
-
-  std::cout << "DefaultConstructorCount: "
-            << TestType::smDefaultConstructorCount << std::endl
-            << "MoveConstructorCount: " << TestType::smMoveConstructorCount
-            << std::endl
-            << "ConstructorCount: " << TestType::smConstructorCount << std::endl
-            << "DestructorCount: " << TestType::smDestructorCount << std::endl
-            << std::endl;
+  TestType::PrintCounts();
 }
 
 void Empty()
 {
   // Copy an empty vector and make sure both vectors are empty.
-  std::cout << "<= Empty =>" << std::endl;
+  std::cout << "<= Empty =>\n";
   Ds::Vector<TestType> empty;
   Ds::Vector<TestType> notEmpty(empty);
-  std::cout << "Empty: " << (empty.Empty() && notEmpty.Empty()) << std::endl;
+  std::cout << "Empty: " << (empty.Empty() && notEmpty.Empty()) << '\n';
+}
+
+void RunTest(void (*test)())
+{
+  static bool firstTest = true;
+  if (firstTest) {
+    firstTest = false;
+  }
+  else {
+    std::cout << '\n';
+  }
+  TestType::ResetCounts();
+  test();
 }
 
 int main(void)
 {
   EnableLeakOutput();
-  CopyConstructor0();
-  CopyConstructor1();
-  MoveConstructor();
-  SinglePush();
-  MovePush();
-  MultiplePush();
-  Emplace();
-  Insert0();
-  Insert1();
-  Insert2();
-  Pop();
-  Clear();
-  Remove0();
-  Remove1();
-  LazyRemove();
-  IndexOperator();
-  CopyAssignment0();
-  CopyAssignment1();
-  CopyAssignment2();
-  CopyAssignment3();
-  MoveAssignment();
-  Find();
-  Contains();
-  Resize0();
-  Resize1();
-  Resize2();
-  Reserve();
-  Shrink0();
-  Shrink1();
-  CData();
-  Top();
-  InnerVector();
-  BigInnerVector();
-  ConstructionDestructionCounts();
-  Empty();
+  RunTest(CopyConstructor0);
+  RunTest(CopyConstructor1);
+  RunTest(MoveConstructor);
+  RunTest(SinglePush);
+  RunTest(MovePush);
+  RunTest(MultiplePush);
+  RunTest(Emplace);
+  RunTest(Insert0);
+  RunTest(Insert1);
+  RunTest(Insert2);
+  RunTest(Pop);
+  RunTest(Clear);
+  RunTest(Remove0);
+  RunTest(Remove1);
+  RunTest(LazyRemove);
+  RunTest(IndexOperator);
+  RunTest(CopyAssignment0);
+  RunTest(CopyAssignment1);
+  RunTest(CopyAssignment2);
+  RunTest(CopyAssignment3);
+  RunTest(MoveAssignment);
+  RunTest(Find);
+  RunTest(Contains);
+  RunTest(Resize0);
+  RunTest(Resize1);
+  RunTest(Resize2);
+  RunTest(Reserve);
+  RunTest(Shrink0);
+  RunTest(Shrink1);
+  RunTest(CData);
+  RunTest(Top);
+  RunTest(InnerVector);
+  RunTest(BigInnerVector);
+  RunTest(ConstructionDestructionCounts);
+  RunTest(Empty);
 }
