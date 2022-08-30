22bc675c5df2f2e4aacac22c4aa79dddbc8257dc
Underdisc <connortdeakin@gmail.com>
2022-08-30 11:40:37 +0200

    Increase the breadth of debug infromation devlivered to stdout from crt.
    Instead of only sending crt memory warnings to stdout, we now send errors and asserts as well. This gives more debugging information for memory issues. Buffer overrun is one example of the new debugging information.
    
    Disabling leak output also undoes all of the work done by the enable. This was not the case beforehand.

--- src/debug/MemLeak.cc
+++ src/debug/MemLeak.cc
@@ -5,6 +5,10 @@ void EnableLeakOutput()
 #if defined WIN32 && defined _DEBUG
   _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
   _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 }
@@ -13,6 +17,10 @@ void DisableLeakOutput()
 {
 #if defined WIN32 && defined _DEBUG
   _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_DEBUG);
+  int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+  flags &= ~(_CRTDB_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+  _CrtSetDbgFlag(flags);
 #endif
 }
