b39246df6a72f1413aecf23e156bd9384c4838f6
Underdisc <connortdeakin@gmail.com>
2022-10-17 14:49:04 +0200

    Update to the cubemap resource for the asset refactor.

--- src/gfx/Cubemap.cc
+++ src/gfx/Cubemap.cc
@@ -1,107 +1,32 @@
+#include <imgui/imgui.h>
 #include <stb_image.h>
 #include <utility>
 
-#include "AssetLibrary.h"
+#include "editor/Utility.h"
 #include "gfx/Cubemap.h"
+#include "rsl/Library.h"
 
 namespace Gfx {
 
-void Cubemap::InitInfo::Prep(const std::string& file)
+Cubemap::FilterType Cubemap::GetFilterType(const std::string& filterTypeString)
 {
-  mFilter = GL_LINEAR;
-  for (int i = 0; i < 6; ++i) {
-    mFiles[i] = file;
-  }
-}
-
-void Cubemap::InitInfo::Serialize(Vlk::Value& val) const
-{
-  for (int i = 0; i < 6; ++i) {
-    val(smFileDescriptors[i]) = mFiles[i];
-  }
-  Vlk::Value& filterVal = val("Filter");
-  switch (mFilter) {
-  case GL_LINEAR: filterVal = smFilterTypes[0];
-  case GL_NEAREST: filterVal = smFilterTypes[1];
-  }
-}
-
-void Cubemap::InitInfo::Deserialize(const Vlk::Explorer& ex)
-{
-  for (int i = 0; i < 6; ++i) {
-    mFiles[i] = ex(smFileDescriptors[i]).As<std::string>("");
-  }
-  std::string filterType = ex("Filter").As<std::string>(smFilterTypes[0]);
-  if (filterType == smFilterTypes[1]) {
-    mFilter = GL_LINEAR;
-  }
-  else {
-    mFilter = GL_NEAREST;
-  }
-}
-
-Result Cubemap::Init(const std::string& file)
-{
-  // This Init function was made specifically for the default cubemap. It uses
-  // the same 9 pixel image for each side.
-  InitInfo info;
-  for (int i = 0; i < 6; ++i) {
-    info.mFiles[i] = file;
-  }
-  info.mFilter = GL_NEAREST;
-  return Init(info);
-}
-
-Result Cubemap::Init(const InitInfo& info)
-{
-  Purge();
-
-  glGenTextures(1, &mId);
-  glBindTexture(GL_TEXTURE_CUBE_MAP, mId);
-  unsigned char* data;
-  for (int i = 0; i < 6; ++i) {
-    // Resolve the path for the current face.
-    ValueResult<std::string> resolutionResult =
-      AssLib::ResolveResourcePath(info.mFiles[i]);
-    if (!resolutionResult.Success()) {
-      return Result(resolutionResult.mError);
+  for (int i = 0; i < (int)FilterType::Count; ++i) {
+    if (filterTypeString == smFilterTypeStrings[i]) {
+      return (FilterType)i;
     }
-    std::string path = resolutionResult.mValue;
-
-    // Upload the texture for the current face.
-    int width, height, channels;
-    stbi_set_flip_vertically_on_load(false);
-    data = stbi_load(path.c_str(), &width, &height, &channels, 0);
-    if (!data) {
-      std::stringstream error;
-      error << "Failed to load \"" << path << "\": " << stbi_failure_reason();
-      return Result(error.str());
-    }
-    GLenum face = GL_TEXTURE_CUBE_MAP_POSITIVE_X + i;
-    GLenum format;
-    if (channels == 4) {
-      format = GL_RGBA;
-    }
-    else {
-      format = GL_RGB;
-    }
-    glTexImage2D(
-      face, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-    stbi_image_free(data);
   }
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, info.mFilter);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, info.mFilter);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-  glFinish();
-  return Result();
+  return FilterType::Invalid;
 }
 
-void Cubemap::Purge()
+Cubemap::Specification Cubemap::GetSpecification(
+  const std::string& specificationString)
 {
-  glDeleteTextures(1, &mId);
-  mId = 0;
+  for (int i = 0; i < (int)Specification::Count; ++i) {
+    if (specificationString == smSpecificationStrings[i]) {
+      return (Specification)i;
+    }
+  }
+  return Specification::Invalid;
 }
 
 Cubemap::Cubemap(): mId(0) {}
@@ -120,12 +45,215 @@ Cubemap& Cubemap::operator=(Cubemap&& other)
 
 Cubemap::~Cubemap()
 {
-  Purge();
+  glDeleteTextures(1, &mId);
+  mId = 0;
+}
+
+Result Cubemap::Init(const Vlk::Explorer& configEx)
+{
+  Config config;
+
+  // Get the filter type.
+  Vlk::Explorer filterEx = configEx("Filter");
+  if (!filterEx.Valid(Vlk::Value::Type::TrueValue)) {
+    return Result("Missing :Filter: TrueValue");
+  }
+  std::string filterTypeString = filterEx.As<std::string>();
+  FilterType filterType = GetFilterType(filterTypeString);
+  if (filterType == FilterType::Invalid) {
+    std::string error = "Filter \"" + filterTypeString + "\" is invalid.";
+    return Result(error);
+  }
+  config.mGlFilter = smFilterTypeGlValues[(int)filterType];
+
+  // Get the specification type.
+  Vlk::Explorer specificationEx = configEx("Specification");
+  if (!specificationEx.Valid(Vlk::Value::Type::TrueValue)) {
+    return Result("Missing :Specification: TrueValue");
+  }
+  std::string specificationString = specificationEx.As<std::string>();
+  if (config.mSpecification == Specification::Invalid) {
+    std::string error =
+      "Specification \"" + specificationString + "\" is invalid.";
+    return Result(error);
+  }
+  config.mSpecification = GetSpecification(specificationString);
+
+  // Get the face files depending on specification type.
+  if (config.mSpecification == Specification::Split) {
+    for (int i = 0; i < smFileDescriptorCount; ++i) {
+      Vlk::Explorer fileEx = configEx(smFileDescriptorStrings[i]);
+      if (!fileEx.Valid(Vlk::Value::Type::TrueValue)) {
+        std::string error = "Missing :";
+        error += smFileDescriptorStrings[i];
+        error += ": TrueValue.";
+        return Result(error);
+      }
+      config.mFaceFiles[i] = fileEx.As<std::string>();
+    }
+  }
+  else if (config.mSpecification == Specification::Single) {
+    Vlk::Explorer fileEx = configEx("File");
+    if (!fileEx.Valid(Vlk::Value::Type::TrueValue)) {
+      return Result("Missing :File: TrueValue");
+    }
+    config.mFaceFiles[0] = fileEx.As<std::string>();
+  }
+
+  // We have a valid configuration if we reach this point.
+  Init(config);
+  return Result();
+}
+
+Result Cubemap::Init(const Config& config)
+{
+  auto uploadFace = [](GLenum target, GLenum format, const Face& face)
+  {
+    glTexImage2D(
+      target,
+      0,
+      format,
+      face.mWidth,
+      face.mHeight,
+      0,
+      format,
+      GL_UNSIGNED_BYTE,
+      face.mData);
+  };
+
+  glGenTextures(1, &mId);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, mId);
+  if (config.mSpecification == Specification::Split) {
+    for (int i = 0; i < smFileDescriptorCount; ++i) {
+      ValueResult<Face> result = Face::Init(config.mFaceFiles[i]);
+      if (!result.Success()) {
+        return result;
+      }
+      const Face& face = result.mValue;
+      GLenum target = GL_TEXTURE_CUBE_MAP_POSITIVE_X + i;
+      GLenum format = face.mChannels == 4 ? GL_RGBA : GL_RGB;
+      uploadFace(target, format, face);
+    }
+  }
+  else if (config.mSpecification == Specification::Single) {
+    ValueResult<Face> result = Face::Init(config.mFaceFiles[0]);
+    if (!result.Success()) {
+      return result;
+    }
+    const Face& face = result.mValue;
+    GLenum format = face.mChannels == 4 ? GL_RGBA : GL_RGB;
+    for (int i = 0; i < smFileDescriptorCount; ++i) {
+      GLenum target = GL_TEXTURE_CUBE_MAP_POSITIVE_X + i;
+      uploadFace(target, format, face);
+    }
+  }
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, config.mGlFilter);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, config.mGlFilter);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+  return Result();
+}
+
+void Cubemap::EditConfig(Vlk::Value* configValP)
+{
+  // Edit the filter type.
+  ImGui::PushItemWidth(-Editor::CalcBufferWidth("Specification"));
+  Vlk::Value& configVal = *configValP;
+  Vlk::Value& filterVal = configVal("Filter");
+  std::string filterString = filterVal.As<std::string>(smFilterTypeStrings[0]);
+  FilterType filterType = GetFilterType(filterString);
+  int newFilterType = (int)filterType;
+  ImGui::Combo(
+    "Filter", &newFilterType, smFilterTypeStrings, (int)FilterType::Count);
+  if (newFilterType != (int)filterType) {
+    filterVal = smFilterTypeStrings[newFilterType];
+  }
+
+  // Edit the specification.
+  Vlk::Value& specificationVal = configVal("Specification");
+  std::string specificationString =
+    specificationVal.As<std::string>(smSpecificationStrings[0]);
+  Specification currentSpecification = GetSpecification(specificationString);
+  int newSpecificationIndex = (int)currentSpecification;
+  ImGui::Combo(
+    "Specification",
+    &newSpecificationIndex,
+    smSpecificationStrings,
+    (int)Specification::Count);
+  if (newSpecificationIndex != (int)currentSpecification) {
+    specificationVal = smSpecificationStrings[newSpecificationIndex];
+    currentSpecification = (Specification)newSpecificationIndex;
+  }
+  ImGui::PopItemWidth();
+
+  // Edit the file paths.
+  if (currentSpecification == Specification::Split) {
+    configVal.TryRemovePair("File");
+    float bufferWidth = Editor::CalcBufferWidth(smFileDescriptorStrings[3]);
+    for (int i = 0; i < smFileDescriptorCount; ++i) {
+      Vlk::Value& fileVal = configVal(smFileDescriptorStrings[i]);
+      std::string file = fileVal.As<std::string>("");
+      // todo: This should accept drag and drop input from the file browser.
+      Editor::InputText(smFileDescriptorStrings[i], &file, -bufferWidth);
+      fileVal = file;
+    }
+  }
+  else if (currentSpecification == Specification::Single) {
+    configVal.TryRemovePair("Files");
+    ImGui::Text("File");
+    ImGui::SameLine();
+    Vlk::Value& fileVal = configVal("File");
+    std::string file = fileVal.As<std::string>("");
+    // todo: drag and drop.
+    Editor::InputText("File", &file, -Editor::CalcBufferWidth("File"));
+    fileVal = file;
+  }
 }
 
 GLuint Cubemap::Id() const
 {
   return mId;
+}
+
+ValueResult<Cubemap::Face> Cubemap::Face::Init(const std::string& file)
+{
+  // Resolve the file path.
+  ValueResult<std::string> resolutionResult = Rsl::ResolveResPath(file);
+  if (!resolutionResult.Success()) {
+    return resolutionResult;
+  }
+  const std::string& resolvedFile = resolutionResult.mValue;
+
+  // Get the face data.
+  Face face;
+  stbi_set_flip_vertically_on_load(false);
+  face.mData = stbi_load(
+    resolvedFile.c_str(), &face.mWidth, &face.mHeight, &face.mChannels, 0);
+  if (face.mData == nullptr) {
+    std::string error =
+      "Loading \"" + file + "\" failed.\n" + stbi_failure_reason();
+    return ValueResult<Face>(Result(error));
+  }
+  return ValueResult<Face>(std::move(face));
+}
+
+Cubemap::Face::Face(): mData(nullptr) {}
+
+Cubemap::Face::Face(Face&& other):
+  mData(other.mData),
+  mWidth(other.mWidth),
+  mHeight(other.mHeight),
+  mChannels(other.mChannels)
+{
+  other.mData = nullptr;
+}
+
+Cubemap::Face::~Face()
+{
+  if (mData != nullptr) {
+    stbi_image_free(mData);
+  }
 }
 
 } // namespace Gfx
--- src/gfx/Cubemap.h
+++ src/gfx/Cubemap.h
@@ -2,7 +2,6 @@
 #define gfx_Cubemap_h
 
 #include <glad/glad.h>
-#include <string>
 
 #include "vlk/Valkor.h"
 
@@ -10,32 +9,72 @@ namespace Gfx {
 
 struct Cubemap
 {
-  struct InitInfo
+public:
+  enum class FilterType
   {
-    constexpr static const char* smFileDescriptors[6] {
-      "Right", "Left", "Top", "Bottom", "Back", "Front"};
-    std::string mFiles[6];
-    constexpr static const char* smFilterTypes[2] {"Nearest", "Linear"};
-    GLint mFilter;
-
-    void Prep(const std::string& file);
-    void Serialize(Vlk::Value& val) const;
-    void Deserialize(const Vlk::Explorer& ex);
+    Nearest,
+    Linear,
+    Count,
+    Invalid
   };
+  constexpr static const char* smFilterTypeStrings[(int)FilterType::Count] {
+    "Nearest", "Linear"};
+  constexpr static GLint smFilterTypeGlValues[(int)FilterType::Count] {
+    GL_NEAREST, GL_LINEAR};
+  static FilterType GetFilterType(const std::string& filterTypeString);
 
-  Result Init(const InitInfo& info);
-  void Finalize() {};
-  void Purge();
+  // Determines whether one or six image files are used in the cubemap.
+  enum class Specification
+  {
+    Split,
+    Single,
+    Count,
+    Invalid,
+  };
+  constexpr static const char*
+    smSpecificationStrings[(int)Specification::Count] = {"Split", "Single"};
+  static Specification GetSpecification(const std::string& specificationString);
+
+  // The key strings for image files in a split specification.
+  constexpr static int smFileDescriptorCount = 6;
+  constexpr static const char* smFileDescriptorStrings[smFileDescriptorCount] {
+    "Right", "Left", "Top", "Bottom", "Back", "Front"};
+
+  struct Config
+  {
+    std::string mFaceFiles[smFileDescriptorCount];
+    GLint mGlFilter;
+    Specification mSpecification;
+  };
 
   Cubemap();
   Cubemap(Cubemap&& other);
   Cubemap& operator=(Cubemap&& other);
   ~Cubemap();
 
-  Result Init(const std::string& file);
+  Result Init(const Vlk::Explorer& configEx);
+  Result Init(const Config& config);
+  static void EditConfig(Vlk::Value* configValP);
+
   GLuint Id() const;
 
 private:
+  struct Face
+  {
+  public:
+    static ValueResult<Face> Init(const std::string& file);
+    unsigned char* mData;
+    int mWidth;
+    int mHeight;
+    int mChannels;
+
+  private:
+    Face();
+    Face(Face&& other);
+    ~Face();
+    friend ValueResult<Face>;
+  };
+
   GLuint mId;
 };
 
