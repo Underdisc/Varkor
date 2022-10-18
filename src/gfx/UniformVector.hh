1faaffe2d1b2c0f9421f33b86bf91c6c02094451
Underdisc <connortdeakin@gmail.com>
2022-10-18 12:08:31 +0200

    Initial commit of Gfx::UniformVector.
    The goal of this is to simplify the interface for gpu uniforms. By storing uniform data in a UniformVector, setting the uniforms specified by materials, renderables, and renderable collections can be deferred until just before a render is actually performed. This allows components and projects to make use of custom uniforms without needing to call glUniform* functions directly or with other helper functions.
    
    The usage of this structure will come in the later commits that introduce Gfx::Material and Gfx::Renderable.

---
+++ src/gfx/UniformVector.hh
@@ -0,0 +1,97 @@
+#include "util/Memory.h"
+
+namespace Gfx {
+
+template<typename T>
+UniformTypeId UniformType<T>::smTypeId = UniformTypeId::Invalid;
+
+extern UniformTypeData nUniformTypeData[];
+
+template<typename T>
+void UniformType<T>::RegisterAttached(UniformTypeId typeId, const char* name)
+{
+  if (smTypeId != UniformTypeId::Invalid) {
+    UniformTypeData& typeData = nUniformTypeData[(int)smTypeId];
+    std::string error = "Attached type \"";
+    error += typeData.mName;
+    error += "\" already registered.";
+    LogAbort(error.c_str());
+  }
+  smTypeId = typeId;
+  RegisterDetached(typeId, name);
+}
+
+template<typename T>
+void UniformType<T>::RegisterDetached(UniformTypeId typeId, const char* name)
+{
+  UniformTypeData& typeData = nUniformTypeData[(int)typeId];
+  if (typeData.mName != nullptr) {
+    UniformTypeData& typeData = nUniformTypeData[(int)typeId];
+    std::string error = "Detached type \"";
+    error += typeData.mName;
+    error += "\" already registered.";
+    LogAbort(error.c_str());
+  }
+  UniformTypeData newTypeData;
+  newTypeData.mName = name;
+  newTypeData.mSize = sizeof(T);
+  newTypeData.mDefaultConstruct = Util::DefaultConstruct<T>;
+  newTypeData.mMoveConstruct = Util::MoveConstruct<T>;
+  newTypeData.mDestruct = Util::Destruct<T>;
+  nUniformTypeData[(int)typeId] = newTypeData;
+}
+
+template<typename T>
+UniformTypeId GetUniformTypeId()
+{
+  bool invalid = UniformType<T>::smTypeId == UniformTypeId::Invalid;
+  LogAbortIf(invalid, "Not registered as attached type.");
+  return UniformType<T>::smTypeId;
+}
+
+template<typename T>
+const UniformTypeData& GetUniformTypeData()
+{
+  UniformTypeId typeId = GetUniformTypeId<T>();
+  return GetUniformTypeData(typeId);
+}
+
+template<typename T>
+T& UniformVector::Add(const std::string& name)
+{
+  UniformTypeId typeId = GetUniformTypeId<T>();
+  return *(T*)Add(typeId, name);
+}
+
+template<typename T>
+void UniformVector::Add(const std::string& name, const T& value)
+{
+  UniformTypeId typeId = GetUniformTypeId<T>();
+  T& uniformValue = *(T*)Add(typeId, name);
+  uniformValue = value;
+}
+
+template<typename T>
+void UniformVector::Add(
+  UniformTypeId typeId, const std::string& name, const T& value)
+{
+  T& uniformValue = *(T*)Add(typeId, name);
+  uniformValue = value;
+}
+
+template<typename T>
+T& UniformVector::Get(const std::string& name)
+{
+  UniformTypeId typeId = GetUniformTypeId<T>();
+  return *(T*)Get(typeId, name);
+}
+
+template<typename T>
+void UniformVector::BindUniform(
+  const Shader& shader, const UniformDescriptor& uniformDesc) const
+{
+  char* uniformData = &mData[uniformDesc.mByteIndex];
+  shader.SetUniform(uniformDesc.mName.c_str(), *(T*)uniformData);
+}
+
+} // namespace Gfx
