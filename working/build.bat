0e69c75c3fbfe8d682b091cb7bdfc712420bee87
Underdisc <connortdeakin@gmail.com>
2023-01-21 12:50:39 +0000

    Build outputs are no longer placed in working.
    With the addition of dealing with different build types and now clang, the number of executables placed in the working directory has gotten unreasonable. To address this, all build outputs are kept within the build dir. When the user requests to run the output from the build script, the file is moved into working, ran, and moved back to the directory it came from.

--- working/build.bat
+++ working/build.bat
@@ -1,13 +1,25 @@
 @echo off
-
 REM Usage: build buildType target [r] [args]
 REM buildType - The type of build (dbg, rel, relDbg, relMin).
 REM target - The target to build.
 REM r - The target will run after a successful build.
 REM args - Arguments given when invoking the built executable.
 
+set scriptDir=%~dp0
 set buildType=%1
 set target=%2
+set action=%3
+
+rem Collect arguments for when we run the built target.
+set args=
+:NextArg
+if "%4" == "" goto AllArgsCollected
+set args=%args% %4
+shift
+goto NextArg
+:AllArgsCollected
+
+set buildDir=%scriptDir%/../build/%compilerDir%/%buildType%
 
 REM Ensure that build specifications are set.
 call checkBuildSpecs.bat
@@ -16,7 +28,7 @@ if errorlevel 1 (
 )
 
 REM Build the target.
-pushd "../build/%compilerDir%/%buildType%"
+pushd "%buildDir%"
 ninja %target%
 popd
 if errorlevel 1 (
@@ -24,21 +36,18 @@ if errorlevel 1 (
 )
 
 REM If requested, run the target with the given arguments.
-if "%3" == "r" (
-  setlocal ENABLEDELAYEDEXPANSION
-  set "args=%3"
-  :NextArg
-  if "%4" == "" goto AllArgsCollected
-  set "args=!args! %4"
-  shift
-  goto NextArg
-  :AllArgsCollected
-  %target%_%buildType%.exe %args%
-  endlocal
-  exit /b 0
-)
-if not "%3" == "" (
-  echo Error: %3 is not a valid argument. Only r is valid.
-  exit /b 1
+if "%action%" == "r" (
+  pushd "%buildDir%"
+  move %target%.* %scriptDir% > nul
+  popd
+  pushd "%scriptDir%"
+  %target%.exe %args%
+  move %target%.* %buildDir% > nul
+  popd
+) else (
+  if not "%action%" == "" (
+    echo Error: %action% is not a valid action. Only r is valid.
+    exit /b 1
+  )
 )
 exit /b 0
