Underdisc/Varkor

commit a097185bc76401fe25fd4aa882adfd00b71db6d9

Object picking is possible even when interfaces are hidden.
Since object picking depends on the layer interface being open and it opens and closes the inspector interface, it was necessary to separate showing interfaces from handling interface staging. This is the reason for the addition of the ShowAll() function and the removal of WindowInterface::HandleInterfaces().

src/editor/Interface.cc
@@ -10,7 +10,7 @@ Interface::Interface(): mOpen(true) {}
 
 Interface::~Interface() {}
 
-void Interface::HandleInterfaces()
+void Interface::HandleStaging()
 {
   // Remove all of the closed interfaces. This happens before inserting the
   // StagedInterfaces to account for the same Interface being closed and opened
@@ -47,7 +47,7 @@ void Interface::HandleInterfaces()
   it = mInterfaces.begin();
   itE = mInterfaces.end();
   while (it != itE) {
-    it->mValue->HandleInterfaces();
+    it->mValue->HandleStaging();
     ++it;
   }
 }
@@ -62,6 +62,14 @@ void Interface::PurgeInterfaces()
     ++it;
   }
   mInterfaces.Clear();
+}
+
+void Interface::ShowAll()
+{
+  Show();
+  for (auto it = mInterfaces.begin(); it != mInterfaces.end(); ++it) {
+    it->mValue->ShowAll();
+  }
 }
 
 } // namespace Editor

src/editor/Editor.cc
@@ -154,6 +154,16 @@ void StartFrame()
   Input::SetKeyboardFocus(!io.WantCaptureKeyboard);
 }
 
+void RunInWorlds()
+{
+  LayerInterface* layerInterface =
+    nCoreInterface.FindInterface<LayerInterface>();
+  if (layerInterface == nullptr) {
+    return;
+  }
+  layerInterface->ObjectPicking();
+}
+
 void EndFrame()
 {
   if (nEditorMode) {
@@ -162,10 +172,13 @@ void EndFrame()
   if (Input::KeyPressed(Input::Key::GraveAccent)) {
     nHideInterface = !nHideInterface;
   }
+
+  nCoreInterface.HandleStaging();
+  RunInWorlds();
   if (!nHideInterface) {
     ImGuiDockNodeFlags flags = ImGuiDockNodeFlags_PassthruCentralNode;
     ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), flags);
-    nCoreInterface.HandleInterfaces();
+    nCoreInterface.ShowAll();
   }
   Gizmos::PurgeUnneeded();
   EndImGuiFrame();

src/editor/Interface.h
@@ -28,8 +28,10 @@ protected:
   bool mOpen;
 
 private:
-  virtual void HandleInterfaces();
+  void HandleStaging();
   void PurgeInterfaces();
+  void ShowAll();
+  virtual void Show() = 0;
 
   struct StagedInterface
   {
@@ -39,7 +41,7 @@ private:
   Ds::Map<std::string, Interface*> mInterfaces;
   Ds::Vector<StagedInterface> mStagedInterfaces;
 
-  friend void Run();
+  friend void EndFrame();
   friend void Purge();
   friend WindowInterface;
 };

src/editor/LayerInterface.cc
@@ -13,27 +13,10 @@ LayerInterface::LayerInterface(World::LayerIt layerIt): mLayerIt(layerIt) {}
 
 void LayerInterface::Show()
 {
-  // Handle object picking.
-  World::Space& space = mLayerIt->mSpace;
-  InspectorInterface* inspector = FindInterface<InspectorInterface>();
-  if (!nSuppressObjectPicking && Input::MousePressed(Input::Mouse::Left)) {
-    World::MemberId clickedMemberId =
-      Gfx::Renderer::HoveredMemberId(space, nCamera.View(), nCamera.Proj());
-    bool clickedSelected =
-      inspector != nullptr && clickedMemberId == inspector->mObject.mMemberId;
-    if (clickedSelected) {
-      CloseInterface<InspectorInterface>();
-    }
-    else if (clickedMemberId != World::nInvalidMemberId) {
-      World::Object clickedObject(&space, clickedMemberId);
-      inspector = OpenInterface<InspectorInterface>(clickedObject);
-    }
-  }
-  nSuppressObjectPicking = false;
-
   ImGui::Begin("Layer", &mOpen);
 
   // Allow the user to change the layer's camera with drag and drop.
+  World::Space& space = mLayerIt->mSpace;
   std::stringstream cameraLabel;
   cameraLabel << "Camera: ";
   if (mLayerIt->mCameraId != World::nInvalidMemberId) {
@@ -60,6 +43,7 @@ void LayerInterface::Show()
   }
   ImGui::BeginChild("Members", ImVec2(0, 0), true);
   Ds::Vector<World::MemberId> rootMemberIds = space.RootMemberIds();
+  InspectorInterface* inspector = FindInterface<InspectorInterface>();
   for (int i = 0; i < rootMemberIds.Size(); ++i) {
     DisplayMember(rootMemberIds[i], &inspector);
   }
@@ -78,6 +62,26 @@ void LayerInterface::Show()
     ImGui::EndDragDropTarget();
   }
   ImGui::End();
+}
+
+void LayerInterface::ObjectPicking()
+{
+  if (!nSuppressObjectPicking && Input::MousePressed(Input::Mouse::Left)) {
+    World::Space& space = mLayerIt->mSpace;
+    World::MemberId clickedMemberId =
+      Gfx::Renderer::HoveredMemberId(space, nCamera.View(), nCamera.Proj());
+    InspectorInterface* inspector = FindInterface<InspectorInterface>();
+    bool clickedSelected =
+      inspector != nullptr && clickedMemberId == inspector->mObject.mMemberId;
+    if (clickedSelected) {
+      CloseInterface<InspectorInterface>();
+    }
+    else if (clickedMemberId != World::nInvalidMemberId) {
+      World::Object clickedObject(&space, clickedMemberId);
+      OpenInterface<InspectorInterface>(clickedObject);
+    }
+  }
+  nSuppressObjectPicking = false;
 }
 
 void LayerInterface::DisplayMember(

src/editor/LayerInterface.h
@@ -12,6 +12,7 @@ struct LayerInterface: public WindowInterface
 public:
   LayerInterface(World::LayerIt layerIt);
   void Show();
+  void ObjectPicking();
   World::LayerIt mLayerIt;
 
 private:

src/editor/WindowInterface.cc
@@ -4,10 +4,4 @@ namespace Editor {
 
 WindowInterface::~WindowInterface() {}
 
-void WindowInterface::HandleInterfaces()
-{
-  Interface::HandleInterfaces();
-  Show();
-}
-
 } // namespace Editor

src/editor/WindowInterface.h
@@ -9,7 +9,6 @@ struct WindowInterface: public Interface
 {
 public:
   virtual ~WindowInterface();
-  void HandleInterfaces();
   virtual void Show() = 0;
 };
 

commit de70ef3122aea373fc14609a0a3321ba75da577b

Fixed bug introduced in a097185
The sub interfaces need to be shown before parent interface.

src/editor/Interface.cc
@@ -65,10 +65,10 @@ void Interface::PurgeInterfaces()
 
 void Interface::ShowAll()
 {
-  Show();
   for (auto it = mInterfaces.begin(); it != mInterfaces.end(); ++it) {
     it->mValue->ShowAll();
   }
+  Show();
 }
 
 } // namespace Editor