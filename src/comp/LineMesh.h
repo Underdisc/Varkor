7ca477c840be2cc4f9dbd56cd35d1bcc8034d22f
Underdisc <connortdeakin@gmail.com>
2023-04-15 10:43:17 +0100

    Reintroduced the line mesh generator.
    It was removed in 0167a3d because I was under the impression that it should stay in the project it was being used for. It turns out that it already has a use case in another project.
    
    I don't necessarily want to share every little thing I make that is usable across every project, though. I might introduce another repository for Varkor extensions that will handle this in the future.
    
    Even though it is not a component, I put the LineMesh in the comp directory because I am hoping to convert it into one.

---
+++ src/comp/LineMesh.h
@@ -0,0 +1,25 @@
+#ifndef gfx_LineMesh_h
+#define gfx_LineMesh_h
+
+#include "ds/Vector.h"
+#include "math/Vector.h"
+#include "rsl/ResourceId.h"
+
+enum class TerminalType
+{
+  Flat,
+  Point,
+  CollapsedNormal,
+  CollapsedBinormal,
+  Arrow,
+};
+
+void InitLineMesh(
+  ResId meshId,
+  const Ds::Vector<Vec3> points,
+  float fill,
+  float thickness,
+  TerminalType startTerminal,
+  TerminalType endTerminal);
+
+#endif
