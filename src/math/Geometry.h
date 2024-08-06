2381040a95316c4eade64512304c5abbd7b1b226
Underdisc <connortdeakin@gmail.com>
2024-08-06 23:04:52 +0100

    Moved Ray and Plane out of Geometry.
    This makes Ray and Plane files visible and it separates functionality that is related, but still in need of separation. The next commit will change Geometry.h/cc to Intersection.h/cc.

--- src/math/Geometry.h
+++ src/math/Geometry.h
@@ -2,49 +2,12 @@
 #define math_Geometry_h
 
 #include "math/Box.h"
+#include "math/Plane.h"
+#include "math/Ray.h"
 #include "math/Sphere.h"
 #include "math/Triangle.h"
-#include "math/Vector.h"
 
 namespace Math {
-
-struct Ray
-{
-public:
-  Vec3 mStart;
-
-private:
-  Vec3 mDirection;
-
-public:
-  void InitNormalized(const Vec3& start, const Vec3& normalizedDirection);
-  void StartDirection(const Vec3& start, const Vec3& direction);
-  void Direction(const Vec3& direction);
-  const Vec3& Direction() const;
-  Vec3 At(float t) const;
-  float ClosestTTo(const Vec3& point) const;
-  Vec3 ClosestPointTo(const Vec3& point) const;
-  bool HasClosestTo(const Ray& other) const;
-  float ClosestTTo(const Ray& other) const;
-  Vec3 ClosestPointTo(const Ray& other) const;
-};
-
-struct Plane
-{
-  // A point on the plane.
-  Vec3 mPoint;
-  // A unit vector perpendicular to the plane.
-  Vec3 mNormal;
-
-  Plane();
-  // Defines the plane using three points a, b, and c.
-  Plane(const Vec3& a, const Vec3& b, const Vec3& c);
-  void Init(const Vec3& a, const Vec3& b, const Vec3& c);
-  void InitNormalized(const Vec3& point, const Vec3& normalizedNormal);
-  void PointNormal(const Vec3& point, const Vec3& normal);
-  void Normal(const Vec3& normal);
-  const Vec3& Normal() const;
-};
 
 bool HasIntersection(const Ray& ray, const Plane& plane);
 Vec3 Intersection(const Ray& ray, const Plane& plane);
---
+++ src/math/Ray.cc
@@ -0,0 +1,67 @@
+#include "math/Ray.h"
+
+namespace Math {
+
+void Ray::InitNormalized(const Vec3& start, const Vec3& normalizedDirection)
+{
+  mStart = start;
+  mDirection = normalizedDirection;
+}
+
+void Ray::StartDirection(const Vec3& start, const Vec3& direction)
+{
+  mStart = start;
+  Direction(direction);
+}
+
+void Ray::Direction(const Vec3& direction)
+{
+  mDirection = Normalize(direction);
+}
+
+const Vec3& Ray::Direction() const
+{
+  return mDirection;
+}
+
+Vec3 Ray::At(float t) const
+{
+  return mStart + mDirection * t;
+}
+
+float Ray::ClosestTTo(const Vec3& point) const
+{
+  return Math::Dot(mDirection, point - mStart);
+}
+
+Vec3 Ray::ClosestPointTo(const Vec3& point) const
+{
+  float t = Math::Dot(mDirection, point - mStart);
+  return At(t);
+}
+
+bool Ray::HasClosestTo(const Ray& other) const
+{
+  float directionDot = Math::Dot(mDirection, other.mDirection);
+  return !Math::Near(directionDot, 1.0f) && !Math::Near(directionDot, -1.0f);
+}
+
+float Ray::ClosestTTo(const Ray& other) const
+{
+  Vec3 sd = mStart - other.mStart;
+  const Vec3& da = Direction();
+  const Vec3& db = other.Direction();
+  float dadb = Math::Dot(da, db);
+  float dasd = Math::Dot(da, sd);
+  float dbsd = Math::Dot(db, sd);
+  float denom = 1.0f - dadb * dadb;
+  LogAbortIf(denom == 0.0f, "The rays are parallel.");
+  return (-dasd + dadb * dbsd) / denom;
+}
+
+Vec3 Ray::ClosestPointTo(const Ray& other) const
+{
+  return At(ClosestTTo(other));
+}
+
+} // namespace Math
